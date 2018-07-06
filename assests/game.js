5c3cc4c5f176687648edcd7a8f53fc39dfca39e4
jcastro15 <castrojake99@gmail.com>
2018-07-05 20:16:23 -0400

    first upload

---
+++ assests/game.js
@@ -0,0 +1,63 @@
+
+   var config = {
+    apiKey: "AIzaSyCa9gJBWoWyr7F3iW4RxBPFDO3cmMxATuo",
+    authDomain: "train-eda11.firebaseapp.com",
+    databaseURL: "https://train-eda11.firebaseio.com",
+    projectId: "train-eda11",
+    storageBucket: "train-eda11.appspot.com",
+    messagingSenderId: "294364731164"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
+
+// Clicking submit calls getData function
+$("#submit").on("click", function(event){
+	event.preventDefault();
+  getData();
+})
+
+// Functions ===================================
+
+// This function is called by the submit button being clicked
+// This function gathers data and pushes it to firebase
+function getData() {
+trainName = $("#trainName").val().trim();
+destination = $("#destination").val().trim();
+firstTrain = moment($("#firstTrain").val().trim(), "LT").format("X");
+frequency = $("#frequency").val().trim();
+
+// Pushing info to database
+database.ref().push({
+  trainName : trainName,
+  destination : destination,
+  firstTrain : firstTrain,
+  frequency : frequency
+  });
+
+// Emptying divs after clicking submit
+$("#trainName").val("");
+$("#destination").val("");
+$("#firstTrain").val("");
+$("#frequency").val("");
+
+};
+
+
+// This function notices if a child is added to the database
+// If a child is added the information is then added to the website
+database.ref().on("child_added", function(snapshot){
+var train = snapshot.val().trainName;
+var dest = snapshot.val().destination;
+var fTrain = snapshot.val().firstTrain;
+var freq = parseInt(snapshot.val().frequency);
+var m = Math.ceil(parseInt(moment().diff(moment.unix(fTrain, "X"), 'minutes'))/freq);
+var nextA = moment.unix(fTrain, "X").add(m*freq, "minutes");
+var nextAr= moment(nextA).format("LT");
+var minAway = moment(nextA).diff(moment(), "minutes")+1;
+
+
+
+
+$("#trainTable > tbody").append("<tr><td>" + train + "</td><td>" + dest + "</td><td>" + freq + "</td><td>" + nextAr + "</td><td>" + minAway + "</td></tr>");
+
+});
